342c0d9ca1ee1118e5966ecafa51898f8424298c
Ananth Goyal <ananthgoyal@gmail.com>
2020-01-18 15:12:05 -0800

    newTest

---
+++ src/Page.java
@@ -0,0 +1,35 @@
+
+public class Page {
+
+    private String text;
+
+    public Page(String text){
+        this.text = text;
+    }
+
+    public String getText() {
+        return this.text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String seperate() throws StringIndexOutOfBoundsException {
+        int j = 0;
+        String s = "";
+        for(int i = 0; i < (text.length()-1); i++){
+            s+=(text.substring(i,i+1));
+            j++;
+            if(j>95){
+                if(text.substring(i,i+1).compareTo(" ") == 0) {
+                    s+="\n";
+                    j = 0;
+                }
+
+            }
+
+        }
+        return s;
+    }
+}
--- src/TestDatabase.java
+++ src/TestDatabase.java
@@ -1,2 +1,108 @@
+import java.io.IOException;
+import java.net.MalformedURLException;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import com.gargoylesoftware.htmlunit.*;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import net.sourceforge.htmlunit.corejs.javascript.JavaScriptException;
+import org.jsoup.Jsoup;
+
+
 public class TestDatabase {
+
+    private String link;
+    private String body;
+    private String[] resources;
+
+    public TestDatabase(String link){
+        this.link = link;
+        resources = new String[10];
+    }
+
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+
+    public static void main(String[] args)  throws FailingHttpStatusCodeException, MalformedURLException, IOException, JavaScriptException {
+        TestDatabase yale = new TestDatabase("https://www.google.com/search?domains=yale.edu&sitesearch=avalon.law.yale.edu%2F&q=slave+trade&x=0&y=0");
+        System.out.println(yale.intialRun());
+
+    }
+    public String intialRun() throws FailingHttpStatusCodeException, MalformedURLException, IOException {
+        WebClient webClient = new WebClient(BrowserVersion.CHROME);
+        webClient.getOptions().setJavaScriptEnabled(true);
+        HtmlPage firstPage = webClient.getPage(link);
+        String firstPageContent = firstPage.asXml();
+        this.body = firstPageContent;
+        this.body = this.body.substring(this.body.indexOf("Search Results"));
+        for(int i = 0; i < 9; i++){
+            if((body.contains("<a href=")) && (body.contains(".asp\""))) {
+                resources[i] = this.body.substring(body.indexOf("<a href="), body.indexOf(".asp\""));
+                this.body = this.body.replaceFirst("<a href=", "");
+                this.body = this.body.replaceFirst(".asp\"", "");
+                resources[i] = resources[i].replaceFirst("<a href=", "");
+                resources[i] = resources[i].replaceFirst("\"", "");
+            }
+            else
+                resources[i] = "none";
+            System.out.println(resources[i]);
+        }
+        return secondRun();
+    }
+
+    public String secondRun() throws FailingHttpStatusCodeException, MalformedURLException, IOException {
+        WebClient webClient = new WebClient(BrowserVersion.CHROME);
+        webClient.getOptions().setJavaScriptEnabled(true);
+        HtmlPage firstPage = webClient.getPage(link);
+        WebDriver browser = new HtmlUnitDriver();
+        String rV="";
+        for(int i = 0; i < 9; i++){
+            System.out.println(resources[i]);
+            if(!resources[i].equals("none")) {
+                resources[i] += ".asp";
+                browser.get(resources[i]);
+                Page out = new Page(browser.findElement(By.tagName("body")).getText());
+                rV += out.seperate();
+            }
+            rV+="\n\n";
+            rV+="-----------------------------------------------------------------" +
+                    "---------------------------------------------";
+            rV+="\n";
+            rV+="\n";
+        }
+        browser.close();
+        return rV;
+    }
+
+    public String  html2text(String html) {
+        Page a = new Page( Jsoup.parse(html).text());
+        if(html.contains("<p>"))
+            return a.seperate();
+        return "";
+    }
+
+    public static int ordinalIndexOf(String str, String substr, int n) {
+        int pos = str.indexOf(substr);
+        while (--n > 0 && pos != -1)
+            pos = str.indexOf(substr, pos + 1);
+        return pos;
+    }
+
 }
